25a85fd0d99c8a9aa4c58ca37666ad797d972189
Eray Baran İnan <eraybaraninan@gmail.com>
2021-07-26 19:15:47 +0300

    Initial commit

---
+++ Assets/DoTweenMovement/Editor/DoTweenMovementEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace DoTweenMovement.Editor
+{
+    public class DoTweenMovementEditor : UnityEditor.Editor
+    {
+        [SerializeField] private float closeDistance = 0.1f;
+        [SerializeField] private float speed = 1.0f;
+        [SerializeField] private List<Transform> points;
+        private List<Vector3> positions;
+
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/DoTweenMovement/Scripts/DoTweenMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace DoTweenMovement.Scripts
+{
+    public class DoTweenMovement : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
