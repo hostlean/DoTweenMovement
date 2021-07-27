f42fd9dc17d2ffdc1c1be79fbc351a8aa7789d7d
Eray Baran İnan <eraybaraninan@gmail.com>
2021-07-27 09:01:09 +0300

    Added: Handles

---
+++ Assets/DoTweenMovement/Editor/DoTweenMoveEditor.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using DoTweenMovement.Scripts;
+
+namespace DoTweenMovement.Editor
+{
+    [CustomEditor(typeof(DoTweenMove))]
+    public class DoTweenMoveEditor : UnityEditor.Editor
+    {
+        private DoTweenMove myScript;
+
+        private SerializedProperty closeDistance;
+        private SerializedProperty speedType;
+        private SerializedProperty speed;
+        private SerializedProperty minValue;
+        private SerializedProperty maxValue;
+
+        private SerializedProperty pathPoints;
+
+        private SerializedProperty pointGizmoColor;
+        private SerializedProperty gizmoSize;
+        private SerializedProperty gizmoType;
+        private SerializedProperty pointMoveInEditor;
+
+        private readonly GUILayoutOption minWidth = GUILayout.MinWidth(100);
+        private readonly GUILayoutOption maxWidth = GUILayout.MaxWidth(200);
+
+        AnimationCurve speedCurve = AnimationCurve.Linear(0, 0, 1, 1);
+        
+        private GUIStyle boldStyle = GUIStyle.none;
+        GUIStyle numberStyle = new GUIStyle();
+
+        private Camera _cam;
+
+
+        private void OnEnable()
+        {
+            var cam = SceneView.GetAllSceneCameras();
+            _cam = cam[0];
+            myScript = (DoTweenMove) target;
+            closeDistance = serializedObject.FindProperty(nameof(closeDistance));
+            speedType = serializedObject.FindProperty(nameof(speedType));
+            speed = serializedObject.FindProperty(nameof(speed));
+            minValue = serializedObject.FindProperty(nameof(minValue));
+            maxValue = serializedObject.FindProperty(nameof(maxValue));
+            pathPoints = serializedObject.FindProperty(nameof(pathPoints));
+            pointGizmoColor = serializedObject.FindProperty(nameof(pointGizmoColor));
+            gizmoSize = serializedObject.FindProperty(nameof(gizmoSize));
+            gizmoType = serializedObject.FindProperty(nameof(gizmoType));
+            pointMoveInEditor = serializedObject.FindProperty(nameof(pointMoveInEditor));
+
+
+            numberStyle.normal.textColor = Color.cyan;
+            numberStyle.fontStyle = FontStyle.Bold;
+            //boldStyle = GUIStyle.none;
+            boldStyle.normal.textColor = Color.white;
+            boldStyle.fontStyle = FontStyle.Bold;
+        }
+
+
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(closeDistance);
+            EditorGUILayout.PropertyField(pathPoints);
+            
+            EditorGUILayout.Space(1);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            EditorGUILayout.Space(1);
+            EditorGUILayout.LabelField("Speed", boldStyle);
+            EditorGUILayout.PropertyField(speedType);
+            switch (myScript.speedType)
+            {
+                case DoTweenMove.SpeedType.Constant:
+                    EditorGUILayout.PropertyField(speed);
+                    break;
+                case DoTweenMove.SpeedType.Curve:
+                    EditorGUILayout.PropertyField(minValue);
+                    EditorGUILayout.PropertyField(maxValue);
+                    EditorGUILayout.CurveField("Speed Curve", speedCurve);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            EditorGUILayout.Space(1);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            EditorGUILayout.Space(1);
+            //Reminder new GUIStyle(GUI.skin.label) {fontStyle = FontStyle.Bold}
+            EditorGUILayout.LabelField("Point Editor", boldStyle);
+            EditorGUILayout.PropertyField(pointMoveInEditor);
+            EditorGUILayout.PropertyField(gizmoSize);
+            EditorGUILayout.PropertyField(gizmoType);
+            EditorGUILayout.PropertyField(pointGizmoColor);
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.EndHorizontal();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected virtual void OnSceneGUI()
+        {
+            myScript = (DoTweenMove) target;
+            var position = myScript.transform.position;
+            var myPathPoints = myScript.PathPoints;
+            var pointCount = myScript.PathPoints.Count;
+            Handles.color = myScript.PointGizmoColor;
+            
+            
+
+
+            // if (t.GetOriginalTransformPositionStatus() == false)
+            // {
+            //     return;
+            // }
+
+            for (int i = 0; i < pointCount; i++)
+            {
+                EditorGUI.BeginChangeCheck();
+                
+                Vector3 oldPoint = position + myScript.PathPoints[i].posiiton;
+
+                //Draw handles for points
+                Handles.DrawWireDisc(position + myScript.PathPoints[i].posiiton,
+                    Vector3.forward, 
+                    myScript.GizmoSize);
+                
+                Handles.Label(
+                    position + myScript.PathPoints[i].posiiton + (Vector3.down * 0.4f) +
+                    (Vector3.right * 0.4f), "" + i, numberStyle);
+                
+                Vector3 newPoint = Vector3.zero;
+
+                switch (myScript.PointMoveInEditor)
+                {
+                    case DoTweenMove.GizmoMovementType.Free:
+                        //Draw free move Handle
+                        newPoint = Handles.FreeMoveHandle(oldPoint, Quaternion.identity, .5f,
+                            new Vector3(.25f, .25f, .25f), Handles.CircleHandleCap);
+                        break;
+                    case DoTweenMove.GizmoMovementType.Transform:
+                        //Draw transform arrows
+                        newPoint = Handles.PositionHandle(oldPoint, Quaternion.identity);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+
+                // Records changes and undo them
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(target, "Free Move Handle");
+                    myScript.PathPoints[i].posiiton = newPoint - myScript.transform.position;
+                }
+            }
+            
+            //Draw lines for each point
+            Handles.color = Color.white;
+            if (pointCount > 1)
+            {
+                for (int i = 0; i < pointCount; i++)
+                {
+                    if(i + 1 != pointCount)
+                        Handles.DrawLine(position + myPathPoints[i].posiiton, position + myPathPoints[i+1].posiiton);
+                    else
+                        Handles.DrawLine(position + myPathPoints[i].posiiton, position + myPathPoints[0].posiiton);
+                } 
+            }
+            else
+                Gizmos.DrawLine(position, position + myPathPoints[0].posiiton);
+        }
+    }
+}
---
+++ Assets/DoTweenMovement/Scripts/DoTweenMove.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DoTweenMovement.Scripts
+{
+    public class DoTweenMove : MonoBehaviour
+    {
+        [SerializeField] private float closeDistance = 0.1f;
+        [SerializeField] private float speed = 1.0f;
+        [SerializeField] private float minValue = 0.0f;
+        [SerializeField] private float maxValue = 1.0f;
+
+        public SpeedType speedType;
+        public PathType pathType;
+
+
+        [SerializeField] private List<PathPoint> pathPoints;
+        
+        [SerializeField] private Color pointGizmoColor;
+        [SerializeField, Range(.1f, 10.0f)] private float gizmoSize = 1.0f;
+        [SerializeField] private GizmoType gizmoType;
+        [SerializeField] private GizmoMovementType pointMoveInEditor;
+
+        public GizmoMovementType PointMoveInEditor => pointMoveInEditor;
+
+        private List<Vector3> _positions = new List<Vector3>();
+
+        private int positionCount;
+        private int _currentIndex;
+
+        //Properties
+        public List<PathPoint> PathPoints => pathPoints;
+        public float GizmoSize => gizmoSize;
+        public Color PointGizmoColor => pointGizmoColor;
+
+        public enum SpeedType
+        {
+            Constant,
+            Curve
+        }
+        
+        public enum PathType
+        {
+            Line,
+            Bezier
+        }
+
+        public enum GizmoType
+        {
+            Sphere,
+            Cube,
+            WiredSphere,
+            WiredCube
+        }
+
+        public enum GizmoMovementType
+        {
+            Free,
+            Transform
+        }
+
+        private void Awake()
+        {
+            for (int i = 0; i < pathPoints.Count; i++)
+            {
+                _positions.Add(transform.position + pathPoints[i].posiiton);
+            }
+            
+            positionCount = _positions.Count;
+        }
+
+        private void FixedUpdate()
+        {
+            switch (pathType)
+            {
+                case PathType.Line:
+                    MoveWithLine();
+                    break;
+                case PathType.Bezier:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+           
+        }
+
+        private void MoveWithLine()
+        {
+            transform.position =
+                Vector3.MoveTowards(
+                    transform.position, 
+                    _positions[_currentIndex], 
+                    Time.fixedDeltaTime * speed);
+
+            if (Vector3.Distance(transform.position, _positions[_currentIndex]) < closeDistance)
+            {
+                var nextIndex = _currentIndex + 1;
+                _currentIndex = nextIndex == positionCount ? 0 : nextIndex;
+            }
+            
+        }
+    }
+
+    [Serializable]
+    public class PathPoint
+    {
+        public Vector3 posiiton;
+
+    }
+}
